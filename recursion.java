f4e8d08b5081b2a25a246a5cdc1553c2ad1104f4
KimHak131 <137867485+KimHak131@users.noreply.github.com>
2023-11-23 13:50:55 +0700

    Lab github

--- recursion.java
+++ recursion.java
@@ -1,15 +1,36 @@
+
 public class recursion {
   public static void main(String[] args) {
 
     //call your recursion fuction
     //print result
+    String[] stringsToCheck = {"father", "mother", "madam", "racecar"}; // Add more strings as needed
 
+        for (String str : stringsToCheck) {
+            boolean result = recursionFunction(str);
+            if (result) {
+                System.out.println("'" + str + "' is a palindrome.");
+            } else {
+                System.out.println("'" + str + "' is not a palindrome.");
+            }
+        }
    }
     
-    public double.... recursionFunction {
-      //implement your case algorithm
-      return....
-    }
-  
+    public static boolean recursionFunction(String str) {
+
+        // if string is empty or has one character, it's palindrome
+        if(str.length() <= 1){
+          return true;
+        }        
+
+        // Check if the first and last character are equal
+        if(str.charAt(0) == str.charAt(str.length() - 1)){
+          // Recursively check the substring excluding the first and last characters
+          return recursionFunction(str.substring(1, str.length() - 1));
+          }else{
+            // If characters don't match, it's not a palindrome
+             return false;
+          }
+      }
 }
     
